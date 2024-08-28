159fddfede078330a3e8d8401e1a401d9f2ec725
Erwin Alberto Lopez Hurtado <115179060+Juler115@users.noreply.github.com>
2024-08-28 13:21:12 -0600

    Temporary Commit at 8/28/2024, 1:21:12 PM

--- Erwin/Test/login/src/app/home/home.page.ts
+++ Erwin/Test/login/src/app/home/home.page.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+/** @format */
+
+import { Component } from "@angular/core";
+import {
+	IonHeader,
+	IonToolbar,
+	IonTitle,
+	IonContent,
+	IonIcon,
+	IonCard,
+	IonCardContent,
+	IonCardHeader,
+	IonCardTitle,
+	IonItem,
+	IonLabel,
+	IonInput,
+	IonButton,
+} from "@ionic/angular/standalone";
+import { addIcons } from "ionicons";
+import { logoIonic } from "ionicons/icons";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common"; // Import CommonModule
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+	selector: "app-home",
+	templateUrl: "home.page.html",
+	styleUrls: ["home.page.scss"],
+	standalone: true,
+	imports: [
+		IonHeader,
+		IonToolbar,
+		IonTitle,
+		IonContent,
+		IonIcon,
+		IonCard,
+		IonCardContent,
+		IonCardHeader,
+		IonCardTitle,
+		IonItem,
+		IonLabel,
+		IonInput,
+		IonButton,
+		FormsModule,
+		CommonModule,
+	],
 })
 export class HomePage {
-  constructor() {}
+	greeting: string = "";
+	nameValue: string = "";
+	isSubmitted: boolean = false;
+
+	constructor() {
+		addIcons({ logoIonic });
+	}
+
+	onSubmit() {
+		if (this.nameValue != "") {
+			this.greeting = `Bienvenido, ${this.nameValue}!`;
+			this.isSubmitted = true;
+		}
+	}
 }
